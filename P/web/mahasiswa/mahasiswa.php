4547caa65e0d3593ef3be0b92a8f7ea905b5dfed
yazidnasution02@gmail.com <yazidnasution02@gmail.com>
2025-09-24 21:47:26 +0700

    navigasi.php

--- P/web/mahasiswa/mahasiswa.php
+++ P/web/mahasiswa/mahasiswa.php
@@ -53,34 +53,7 @@ if ($result && mysqli_num_rows($result) > 0) {
       </header>
 
       <aside id="sidebar">
-        <div class="sidebar-title">
-          <div class="sidebar-brand"><span class="material-icons-outlined">inventory</span> Magang Gak jadi</div>
-          <span class="material-icons-outlined" onclick="closeSidebar()">close</span>
-        </div>
-
-        <ul class="sidebar-list">
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank"> <span class="material-icons-outlined">dashboard</span> Dashboard </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="./mitra.php" target="_blank"> <span class="material-icons-outlined">filter_list</span> Daftar Mitra </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="./absensi.php" target="_blank"> <span class="material-icons-outlined">watch</span> Absensi Harian </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank"> <span class="material-icons-outlined">assignment</span> Logbook </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank"> <span class="material-icons-outlined">shopping_cart</span> Sales Orders </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank"> <span class="material-icons-outlined">poll</span> Reports </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank"> <span class="material-icons-outlined">settings</span> Settings </a>
-          </li>
-        </ul>
+        <?php include 'navigasi.php';?>
       </aside>
 
       <main class="main-container">
---
+++ P/web/mitra/navigasi.php
@@ -0,0 +1,25 @@
+<div class="sidebar-title">
+        <a href="dashboard_mitra.php" class="sidebar-brand-link">
+          <div class="sidebar-brand">
+            <span class="material-icons-outlined">business</span> Dashboard Mitra
+          </div>
+        </a>
+        <span class="material-icons-outlined" onclick="closeSidebar()">close</span>
+      </div>
+      <ul class="sidebar-list">
+        <li class="sidebar-list-item">
+          <a href="dashboard_mitra.php?page=input_lowongan">
+            <span class="material-icons-outlined">post_add</span> Input Lowongan PKL
+          </a>
+        </li>
+        <li class="sidebar-list-item">
+          <a href="dashboard_mitra.php?page=pendaftaran">
+            <span class="material-icons-outlined">group</span> Pendaftaran Mahasiswa
+          </a>
+        </li>
+        <li class="sidebar-list-item">
+          <a href="dashboard_mitra.php?page=persetujuan">
+            <span class="material-icons-outlined">check_circle</span> Persetujuan Pendaftaran
+          </a>
+        </li>
+      </ul>
